e7175ae990b5b3b9b31c9f6692dcb6301b2aef73
MohitKS5 <mohitkumarsingh907@gmail.com>
2017-05-12 00:49:45 +0530

    adding alumni page

--- js/pages.js
+++ js/pages.js
@@ -6,33 +6,6 @@ var shrink= function shrink(argument) {
 	$(argument).stop().addClass("shrink");
 };
 
-/*count=1;
-function counter() {
-		count=(count+1)%3;
-		return count;
-	};
-	function counterrev(argument) {
-		count=(count+2)%3;
-	};
-$(window).load(function(){
-	
-	
-		$(".subs> div:gt(0)").hide();
-		$('.righty').click(function(){
-			alert(count);
-		
-			$('.persons:nth-of-type('+count+')').fadeOut(1000).next(".persons").fadeIn(1000);
-			count=counter();
-			
-		});
-
-		$('.lefty').click(function(){
-			alert(count);
-			$('.persons:nth-of-type(3)').fadeOut(1000).prev(".persons").fadeIn(1000);
-		});
-});
-
-*/
    
     $(window).load(function(){
       $('.flexslider').flexslider({
@@ -45,7 +18,20 @@ $(window).load(function(){
           $('body').removeClass('loading');
         }
       });
-    });
+
+
+/*$(".tab-content").hide();
+	$(".tab-content").first().show();
+
+	$("div.tabs li").click(function () {
+	   $("div.tabs li").removeClass("active");
+	   $(this).addClass("active");
+	   $(".tab-content").hide();
+	   var activeTab = $(this).attr("data-id");
+	   $("#" + activeTab).fadeIn(700);
+	});*/
+
+});
   
 
 
