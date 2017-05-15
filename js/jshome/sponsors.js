11a916a4ec81f8781c18ba2a8f7c0d9f246a71fd
MohitKS5 <mohitkumarsingh907@gmail.com>
2017-05-16 00:21:54 +0530

    removing images

--- js/jshome/sponsors.js
+++ js/jshome/sponsors.js
@@ -20,6 +20,16 @@ jQuery(document).ready(function($){
 	            secondaryNav.addClass('animate-children');
 	            $('#cd-logo').addClass('slide-in');
 				$('.cd-btn').addClass('slide-in');
+				$('#tabs').css(
+				{
+					"background-color" : "white"
+				});
+				$('#tabs>a').css({"color" : "black",
+					"letter-spacing" : "1px",
+					"font-size" : "0.8rem" ,
+				 });
+				$('.soc-links').fadeIn(500);
+				$('#home').css({"margin-left" : "20%"});
 	        }, 50);
 		} else {
 			secondaryNav.removeClass('is-fixed');
@@ -28,6 +38,17 @@ jQuery(document).ready(function($){
 	            secondaryNav.removeClass('animate-children');
 	            $('#cd-logo').removeClass('slide-in');
 				$('.cd-btn').removeClass('slide-in');
+				$('#tabs').css(
+				{
+					"background-color" : "black"
+				});
+				$('#tabs>a').css({
+					"color" : "white",
+					"letter-spacing" : "3px",
+					"font-size" : "1rem" 
+					 });
+				$('.soc-links').fadeOut();
+				$('#home').css({"margin-left" : "10%"});
 	        }, 50);
 		}
 
@@ -49,27 +70,5 @@ jQuery(document).ready(function($){
 	}
 
 	//on mobile - open/close secondary navigation clicking/tapping the .cd-secondary-nav-trigger
-	$('.cd-secondary-nav-trigger').on('click', function(event){
-		event.preventDefault();
-		$(this).toggleClass('menu-is-open');
-		secondaryNav.find('ul').toggleClass('is-visible');
-	});
-
-	//smooth scrolling when clicking on the secondary navigation items
-	secondaryNav.find('ul a').on('click', function(event){
-        event.preventDefault();
-        var target= $(this.hash);
-        $('body,html').animate({
-        	'scrollTop': target.offset().top - secondaryNav.height() + 1
-        	}, 400
-        ); 
-        //on mobile - close secondary navigation
-        $('.cd-secondary-nav-trigger').removeClass('menu-is-open');
-        secondaryNav.find('ul').removeClass('is-visible');
-    });
-
-    //on mobile - open/close primary navigation clicking/tapping the menu icon
-	$('.cd-primary-nav').on('click', function(event){
-		if($(event.target).is('.cd-primary-nav')) $(this).children('ul').toggleClass('is-visible');
-	});
+	
 });
