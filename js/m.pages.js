2acf3c5ff9a2fa87e4eb1d12bc963bd232e76122
MohitKS5 <mohitkumarsingh907@gmail.com>
2017-05-31 00:15:47 +0530

    adding home_page

--- js/m.pages.js
+++ js/m.pages.js
@@ -2,4 +2,8 @@ $('.waves-effect').on('click',function (event) {
 	var b=$(this).attr('name');
 	event.preventDefault();
 	$('li.indicator-item:nth-child('+b+')').trigger('click');
-})
+});
+
+$('body').on('click',function () {
+		$('#_homepre').fadeOut(1000);
+});
