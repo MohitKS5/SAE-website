2f82fb562b55c41b51546563aa97b1c54d5d5fff
MohitKS5 <mohitkumarsingh907@gmail.com>
2017-05-31 02:26:22 +0530

    solved scrolling problem

--- js/m.pages.js
+++ js/m.pages.js
@@ -1,9 +1,30 @@
-$('.waves-effect').on('click',function (event) {
+/*$('.waves-effect').on('click',function (event) {
 	var b=$(this).attr('name');
 	event.preventDefault();
 	$('li.indicator-item:nth-child('+b+')').trigger('click');
-});
+});*/
 
 $('body').on('click',function () {
 		$('#_homepre').fadeOut(1000);
 });
+
+
+$(document).ready(function () {
+	$('.carouseld-item').not(':nth-child(1)').hide();
+	// body...
+})
+//carousel bottom navbar navigatio controls
+    $("a.waves-effect").click(function (event) {
+
+        $(".carouseld-item").hide();
+        var activeTab = $(this).text();
+        console.log(activeTab);
+        $("#_" + activeTab).fadeIn(700);
+        var SNT =$('body').offset().top;
+  
+        //changing absolute body backgrounds
+        //$('body').attr('class', '_b'+a);
+            $('html, body').stop().animate({
+              'scrollTop' : SNT
+            },800);
+        });
