f5fe560257f426f754a22621e44ebdf551da3a43
MohitKS5 <mohitkumarsingh907@gmail.com>
2017-07-01 23:43:34 +0530

    final

--- imageload.js
+++ imageload.js
@@ -1,4 +1,4 @@
 $(window).load(function(){
-    $('img').not('.logo>img').each(function(){
+    $('img').not('.logo>img,img.nope').each(function(){
         $(this).attr('src', $(this).attr('lsrc')); });
 });
