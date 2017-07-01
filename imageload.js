6f8935b44e02b2b4a58e0f64c5319716f8918518
MohitKS5 <mohitkumarsingh907@gmail.com>
2017-07-01 21:06:13 +0530

    callback for order of image load

--- imageload.js
+++ imageload.js
@@ -1,6 +1,9 @@
-$(window).load(function(){
-    $('.logo>img').each(function(){
-        $(this).attr('src', $(this).attr('lsrc')); });
-    $('img').each(function(){
-        $(this).attr('src', $(this).attr('lsrc')); });
+$(window).load(function() {
+    $('.logo>img').each(function () {
+        $(this).attr('src', $(this).attr('lsrc')).fadeIn(1000, function () {// will first fade in the logos
+            $('img').each(function () {
+                $(this).attr('src', $(this).attr('lsrc'));// will fade in other images
+            });
+        });
+    });
 });
