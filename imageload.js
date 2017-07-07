1ed1d8810ad6d51cff721f6584e053d6ad8dedcd
MohitKS5 <mohitkumarsingh907@gmail.com>
2017-07-07 21:10:02 +0530

    data fetching variable

--- imageload.js
+++ imageload.js
@@ -1,4 +1,4 @@
 $(window).load(function(){
     $('img').not('.logo>img,img.nope').each(function(){
-        $(this).attr('src', $(this).attr('lsrc')); });
+        $(this).attr('src', "https://res.cloudinary.com/imagelibraryfsae/image/upload/v1498843998$"+(this).attr('lsrc')); });
 });
